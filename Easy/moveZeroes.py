8a57a8379a6170d590ec09243c09df5297fe8b3c
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-09 21:44:46 +0200

    move zeroes

--- Easy/moveZeroes.py
+++ Easy/moveZeroes.py
@@ -7,3 +7,12 @@
 # Example 2:
 # Input: nums = [0]
 # Output: [0]
+
+def moveZeroes(nums):
+    l = 0
+    for r in range(len(nums)):
+        if nums[r] != 0:
+            nums[l], nums[r] == nums[r], nums[l]
+            l += 1
+    
+    return nums
