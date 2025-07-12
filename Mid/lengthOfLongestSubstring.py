270a4b5920314aa384862215068a42352a56f7db
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-12 21:03:39 +0200

    move zeroes

--- Mid/lengthOfLongestSubstring.py
+++ Mid/lengthOfLongestSubstring.py
@@ -8,3 +8,22 @@
 # Input: s = "bbbbb"
 # Output: 1
 # Explanation: The answer is "b", with the length of 1.
+
+def lengthOfLongestSubstring(s):
+    n = len(s)
+    seen = set()
+    l = 0
+    longest = 0
+
+    for r in range(n):
+        while s[r] in seen:
+            seen.remove(s[l])
+            l += 1
+        seen.add(s[r])
+        w = r - l + 1
+        longest = max(longest, w)
+
+    return longest
+
+
+print(lengthOfLongestSubstring("abcabcghdjakbb"))
