5166b8ac3869d30cdab39927fbb0ac4239ab9308
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-20 15:54:22 +0200

    word  search

--- Mid/wordSearch.py
+++ Mid/wordSearch.py
@@ -5,3 +5,41 @@
 # Example 1:
 # Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
 # Output: true
+
+def exist(board, word):
+    m,n = len(board), len(board[0])
+    t = len(word)
+
+    def dfs(i,j):
+        find = 0
+        stack = [(i,j)]
+        seen = set()
+
+        while stack:
+            if find == t:
+                return True
+            
+            x,y = stack.pop()
+
+            if x >= 0  and x < m and y >= 0 and y < n and (x,y) not in seen and board[x][y] == word[find]:
+                find += 1
+                seen.add((x,y))
+
+                stack.append((x+1,y))
+                stack.append((x-1,y))
+                stack.append((x,y+1))
+                stack.append((x,y-1))
+
+
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == word[0] and dfs(i, j):
+                return True
+
+    return False
+
+board = [["A","B","C","E"],
+         ["S","F","C","S"],
+         ["A","D","E","E"]]
+
+print(exist(board, "ABFCS"))
