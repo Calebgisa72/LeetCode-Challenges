8b2acd7a4a9fd58cebc93bc2da9493cf4f2a2a0e
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-21 23:15:19 +0200

    combination sum

--- Mid/combinationSum.py
+++ Mid/combinationSum.py
@@ -3,31 +3,13 @@
 # The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
 # The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
 
-# Example 1:
-# Input: candidates = [2,3,6,7], target = 7
-# Output: [[2,2,3],[7]]
-# Explanation:
-# 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
-# 7 is a candidate, and 7 = 7.
-# These are the only two combinations.
 
-# Example 2:
+# Example 1:
 # Input: candidates = [2,3,5], target = 8
 # Output: [[2,2,2,2],[2,3,3],[3,5]]
 
 def combinationSum(candidates, target):
     n = len(candidates)
-    res,sol = [],[]
-    # def backtrack(total):
-    #     if total == target:
-    #         res.append(sol[:])
-    #         return
-    #     if total > target:
-    #         return
-        
-    #     for i in range
+    
 
-    # backtrack(0)
-    return res
-
-print(combinationSum([2,3,5],4))
+print(combinationSum([2,3,5],8))
