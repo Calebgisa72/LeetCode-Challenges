5d4f0395d345f41a982934567abd4555ec73eda3
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-28 22:29:02 +0200

    maximum k average

--- Easy/maximumKAverage.py
+++ Easy/maximumKAverage.py
@@ -3,7 +3,7 @@
 # Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 
 # Example 1:
-# Input: nums = [1,12,-5,-6,50,3], k = 4
+# Input: nums = [1,12,-5,-6,50,3], k = 
 # Output: 12.75000
 # Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
 
