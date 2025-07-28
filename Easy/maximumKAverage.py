b7501409550ca2469e77272423614811d44b4527
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-07-28 22:29:46 +0200

    maximum k average

--- Easy/maximumKAverage.py
+++ Easy/maximumKAverage.py
@@ -5,7 +5,7 @@
 # Example 1:
 # Input: nums = [1,12,-5,-6,50,3], k = 4
 # Output: 12.75000
-# Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+# Explanation: Maximum average is (12 - 5 - 6 + 0) / 4 = 51 / 4 = 12.75
 
 def findMaxAverage(nums, k):
     currentSum = sum(nums[:k])
