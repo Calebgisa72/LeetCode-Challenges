7b57ac413023f0d03d4c130bc2d6a7e6a2510830
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-08 21:54:09 +0200

    Find Closest Number to Zero

--- Easy/findClosestNumber.py
+++ Easy/findClosestNumber.py
@@ -1,3 +1,7 @@
 # 2239. Find Closest Number to Zero
 # Given an integer array nums of size n, return the number with the value closest to 0 in nums. If there are multiple answers, return the number with the largest value.
 
+# Example 2:
+# Input: nums = [2,-1,1]
+# Output: 1
+# Explanation: 1 and -1 are both the closest numbers to 0, so 1 being larger is returned.
