b919912c52c8aa6bacec9fbb9ef5db53e6c2a010
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-11 19:46:59 +0200

    largest number

--- Mid/largestNumber.py
+++ Mid/largestNumber.py
@@ -11,34 +11,6 @@
 # Output: "9534330"
 
 def largestNumber(nums):
-    stack = []
-    largeNum = '0'
-
-    for num in nums:
-        stack.append([num])
-
-    while stack:
-        numsCopy: list = nums[:]
-        top = stack.pop()
-
-        for y in top:
-            numsCopy.remove(y)
-
-        if len(top) == len(nums):
-            s = ''.join(map(str, top))
-            print(f's {s}')
-
-            if int(s) > int(largeNum):
-                largeNum = s
-            continue
-
-        for num in numsCopy:
-            print(stack)
-            newT = top[:]
-            newT.append(num)
-            stack.append(newT)
-               
     
-    return largeNum
 
 print(largestNumber([0,9,8,7,6,5,4,3,2,1]))
