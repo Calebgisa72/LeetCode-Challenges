ba114779a7be4269ca84b03ce79190c08cc7bf6a
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-11 19:47:09 +0200

    largest number

--- Mid/largestNumber.py
+++ Mid/largestNumber.py
@@ -11,6 +11,21 @@
 # Output: "9534330"
 
 def largestNumber(nums):
+    nums_str = list(map(str, nums))
     
+    # Custom comparator
+    def compare(a, b):
+        if a + b > b + a:
+            return -1
+        elif a + b < b + a:
+            return 1
+        else:
+            return 0
+    
+    # Sort with the custom comparator
+    nums_str.sort(key=cmp_to_key(compare))
+    
+    # Join the result
+    result = ''.join(nums_str)
 
 print(largestNumber([0,9,8,7,6,5,4,3,2,1]))
