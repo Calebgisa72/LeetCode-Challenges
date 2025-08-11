d666f7e6746ba7d9cfc16ca0c27902598cf2c428
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-08-11 20:09:46 +0200

    merge alternately

--- Easy/mergeAlternately.py
+++ Easy/mergeAlternately.py
@@ -10,3 +10,22 @@
 # word1:  a   b   c
 # word2:    p   q   r
 # merged: a p b q c r
+
+def mergeAlternately(word1, word2):
+    from collections import deque
+    w1 = deque(word1)
+    w2 = deque(word2)
+    result = []
+
+    while w1 and w2:
+        result.append(w1.popleft())
+        result.append(w2.popleft())
+    
+    if w1:
+        result += list(w1)
+    if w2:
+        result += list(w2)
+
+    return result
+
+print(mergeAlternately([1,3,5],[2,4,6,7,8,9]))
